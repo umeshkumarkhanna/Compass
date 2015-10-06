75167c59cbbefd5c06d33eeac48ec2e7163707ff
Umesh Kumar <umeshkumarkhanna@hotmail.com>
2015-10-06 07:55:24 -0400

    everything is done, time to remove tops

--- app/src/main/java/ca/umeshkhanna/compass/MyData.java
+++ app/src/main/java/ca/umeshkhanna/compass/MyData.java
@@ -6,12 +6,12 @@ www.101apps.co.za*/
 public class MyData {
 
 
-    static String[] nameArray = {"3 Haviland Circle", "200 University Ave", "Barry", "Willy", "Wally", "Solly", "Dolly", "Molly", "Pete", "Liz"};
+    static String[] nameArray = {"100 Pierrepont Street", "5th Avenue", "166 Perry St", "740 Park Avenue, Unit 4/5D", "101 Warren Street, Unit 3210", "West 37th Street", "Mulberry Street", "1 York Street", "Park Place", "Furman Street"};
     static String[] addressArray = {"Harry", "Sally", "Barry", "Willy", "Wally", "Solly", "Dolly", "Molly", "Pete", "Liz"};
    //Should be double
-   static int[] numOfBedArray = {4, 5, 13, 4, 5, 13, 4, 5, 13, 10};
-    static String[] numOfBathArray = {"4.5", "5", "5.5", "6", "2","4.5", "5", "5.5", "6", "2"};
-    static String[] priceArray = {"4,500,000", "5,760,000", "5,110,000", "6,130,000", "27,000,000","4,200,000", "5,000,000", "450,000", "600,000", "121,000"};
+   static int[] numOfBedArray = {5, 2, 3, 5, 7, 2, 4, 4, 5, 6};
+    static String[] numOfBathArray = {"6", "4.5", "3.5", "5.5", "7","2.5", "4.5", "4.5", "6.5", "7"};
+    static String[] priceArray = {"11,250,000", "30,000,000", "7,500,000", "27,500,000", "29,000,000","4,200,000", "23.750,000", "18,000,000", "29,500,000", "32,000,000"};
     static String[] contactPersonArray = {"Harry", "Sally", "Barry", "Willy", "Wally", "Solly", "Dolly", "Molly", "Pete", "Liz"};
     static String[] contactPersonNumberArray = {"Harry", "Sally", "Barry", "Willy", "Wally", "Solly", "Dolly", "Molly", "Pete", "Liz"};
     static Integer[] drawableArray = {R.drawable.img1, R.drawable.img2, R.drawable.img3,
---
+++ app/src/main/java/ca/umeshkhanna/compass/SplashActivity.java
@@ -0,0 +1,61 @@
+package ca.umeshkhanna.compass;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.CountDownTimer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        new CountDownTimer(3000, 1000) {
+
+            public void onTick(long millisUntilFinished) {
+
+            }
+
+            public void onFinish() {
+                Context context = getApplicationContext();
+                CharSequence text = "Welcome to a mockup of Compass!";
+                int duration = Toast.LENGTH_LONG;
+
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        }.start();
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_splash, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
